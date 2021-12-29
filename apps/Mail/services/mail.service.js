b9c722fc21552b815645ebcd02d791ace422c9f5
Yuval <yuvalshai95@gmail.com>
2021-12-29 13:54:22 +0200

    save dummy data emails to local storage and load to MailApp state all email data

--- apps/Mail/services/mail.service.js
+++ apps/Mail/services/mail.service.js
@@ -1,6 +1,33 @@
 import {utilService} from '../../../services/util.service.js';
 import {storageService} from '../../../services/storage.service.js';
+import {dummyDataService} from './mail.dummydata.service.js';
 
-export const mailService = {};
+export const mailService = {
+  query,
+};
 
-let gEmails = {};
+const STORAGE_KEY = 'emailDB';
+let gEmails = _getEmails();
+
+function query(criteria = null) {
+  console.log('query');
+  let emails = storageService.loadFromStorage(STORAGE_KEY);
+  console.log('emails', emails);
+  if (!criteria) {
+    const inboxEmails = emails.filter(email => email.status === 'inbox');
+    return Promise.resolve(inboxEmails);
+  }
+}
+
+function _getEmails() {
+  let emails = storageService.loadFromStorage(STORAGE_KEY);
+  if (!emails || !emails.length) {
+    emails = dummyDataService.getDummyEmails();
+  }
+  _saveEmailsToStorage(emails);
+  return emails;
+}
+
+function _saveEmailsToStorage(emails) {
+  storageService.saveToStorage(STORAGE_KEY, emails);
+}
--- pages/MailApp.jsx
+++ pages/MailApp.jsx
@@ -1,17 +1,34 @@
+import {mailService} from '../apps/Mail/services/mail.service.js';
+
 export class MailApp extends React.Component {
+  state = {
+    mails: null,
+    criteria: {
+      status: 'inbox',
+      txt: '',
+      isStarred: false,
+      isRead: false,
+    },
+  };
 
-    state = {
-            
-        }
+  componentDidMount() {
+    this.loadMails();
+  }
 
-    render() {
-        
-        return (
-            <section className="keep-app">
-                <h1>Mail app!</h1>
-            </section>
-        )
-    }
+  loadMails = () => {
+    const {criteria} = this.state;
+    mailService.query().then(mails => {
+      this.setState({mails});
+    });
+  };
 
-
+  render() {
+    const {mails} = this.state;
+    console.log('mails', mails);
+    return (
+      <section className="keep-app">
+        <h1>Mail app!</h1>
+      </section>
+    );
+  }
 }
