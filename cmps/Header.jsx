32722e84c025bc0521e236d5de212b4ed1202e11
DanielShaked <89588973+DanielShaked@users.noreply.github.com>
2021-12-29 14:22:42 +0200

    fix bug

--- cmps/Header.jsx
+++ cmps/Header.jsx
@@ -5,7 +5,7 @@ export function Header() {
         <header className="header">
             <div className="header-container main-layout">
                 <Link className="clean-link" to="/">
-                    <h1>Welcome to Appsus</h1>
+                    <h1>Welcome to Appsus!</h1>
                 </Link>
             <nav className="main-nav">
                 <ul className="clean-list flex">
