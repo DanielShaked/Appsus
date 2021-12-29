be65506beffaf538eb7cc8b20d26318ac162b159
DanielShaked <89588973+DanielShaked@users.noreply.github.com>
2021-12-29 09:42:05 +0200

    upload files

---
+++ App.jsx
@@ -0,0 +1,20 @@
+// imports Pages //
+
+// imports cmps //
+
+// import services//
+
+
+
+//routing
+// const Router = ReactRouterDOM.HashRouter
+// const { Route, Switch } = ReactRouterDOM
+
+
+
+export function App() {
+
+  return (
+   <section className="app"></section> 
+  )
+}
---
+++ pages/About.jsx
@@ -0,0 +1,2 @@
+
+// const { NavLink, Route } = ReactRouterDOM
